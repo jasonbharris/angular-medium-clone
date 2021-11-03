e2ad0c1a4aac47665d67d1b807e1455863f3d016
Jason <jason.b.harris1@gmail.com>
2021-11-03 11:31:09 -0500

    added auth module and register plus completed register routing

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,18 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { RouterModule } from "@angular/router";
+
+import { RegisterComponent } from "src/app/auth/components/register/register.component";
+
+const routes = [
+    {
+        path: 'register',
+        component: RegisterComponent
+    }
+]
+
+@NgModule({
+    declarations: [RegisterComponent],
+    imports: [CommonModule, RouterModule.forChild(routes)]
+})
+export class AuthModule {}
