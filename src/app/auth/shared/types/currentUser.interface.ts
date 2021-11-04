263c14c83da9b557e33c21089c9f800386abe34d
Jason <jason.b.harris1@gmail.com>
2021-11-04 13:02:03 -0500

    added interfaces for user and register

---
+++ src/app/auth/shared/types/currentUser.interface.ts
@@ -0,0 +1,9 @@
+export interface CurrentUser {
+    id: number
+    email: string
+    createdAt: string
+    updatedAt: string
+    username: string
+    bio: string | null
+    image: string | null
+}
